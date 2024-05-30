34c787f5acdbb1697ff08d54ca0cb7e4f378c4c3
sokisokisoki <pj33smith@icloud.com>
2024-05-30 18:08:18 -0400

    A few days of work, played around with new APIs and GUIs

--- PlaylistObj.py
+++ PlaylistObj.py
@@ -1,8 +1,11 @@
+import SongObj
+
 class PlaylistObj:
 
     pl_len = 0
     coverUrl = ''
     tracks = []
+    trackObjs = []
 
 
     def __init__(self, name):
@@ -15,11 +18,11 @@ class PlaylistObj:
         self.coverUrl = urlAddr
 
     def addTracks(self, tracklist):
-        self.tracks = []
-        for obj in tracklist:
-            self.tracks.append(obj)
-
+        self.tracks = tracklist
         self.setLen(len(self.tracks))
+
+    def addTrackObj(self, trObj):
+        self.trackObjs = trObj
 
     def displayInfo(self):
         print(self.name)
@@ -30,3 +33,16 @@ class PlaylistObj:
         
     def dispName(self):
         print(self.name)   
+
+    def dispObjs(self):
+        for item in self.trackObjs:
+            item.displayDetail()
+
+    def getSongs(self):
+        return self.trackObjs
+    
+    def getName(self):
+        return self.name
+    
+    def getCoverUrl(self):
+        return self.coverUrl
